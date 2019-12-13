ddba17fe9815d8b094790233c11839c073769098
Alexandre <MeauA26@gmail.com>
2019-12-13 12:22:30 +0100

    ajout d'un lien pour inscription

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
       <li><a href="index.php?page=accueil" title="Page d'accueil">Accueil</a></li>
       <li><a href="index.php?page=sondage" title="Accéder au sondage du jour">Sondage</a></li>
       <li><a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
+      <li><a href="index.php?page=frminscription" title="inscription">inscription</a></li>
       <li><a href="index.php?page=mentions" title="Les mentions légales">Mentions légales</a></li>
     </ul>
   </nav>
